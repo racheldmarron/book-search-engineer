2173b20ee9f9f31b61919672970c4b285e54a59f
Rachel Marron <racheldmarron@gmail.com>
2022-05-25 11:03:58 -0700

    trying to resolve graphql server error

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,6 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useFindAndModify: false,
 });
 
-// ????? 
 // mongoose.connect(
 //   process.env.MONGODB_URI || 'mongodb://localhost:27017/book-search-engineer',
 //   {
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+// const { importSchema } = require('graphql-import')
 
 const db = require('./config/connection'); 
 
